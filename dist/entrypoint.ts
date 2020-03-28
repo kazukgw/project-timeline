6e35a6bc7b4133e0f216dc199fb5fbe34461c508
kazukgw <kazukgw@gmail.com>
2020-03-28 18:26:35 +0900

    Formatted

--- dist/entrypoint.ts
+++ dist/entrypoint.ts
@@ -1,111 +1,147 @@
 const DEBUG = false;
 
 function doGet() {
-    return HtmlService
-        .createTemplateFromFile('index')
-        .evaluate();
+  return HtmlService.createTemplateFromFile("index").evaluate();
 }
 
 function getAllResources(sheetName) {
-    let app = initApp();
-    let groups = app.groupTable.getAllRecords();
-    let projects = app.projectTable.getAllRecords();
-    let schedules = app.scheduleTable.getAllRecords();
-    return JSON.stringify({
-        groups: groups.map((g: TableRecord)=>{return g.values}),
-        projects: projects.map((p: TableRecord)=>{return p.values}),
-        schedules: schedules.map((s: TableRecord)=>{return s.values}),
+  let app = initApp();
+  let groups = app.groupTable.getAllRecords();
+  let projects = app.projectTable.getAllRecords();
+  let schedules = app.scheduleTable.getAllRecords();
+  return JSON.stringify({
+    groups: groups.map((g: TableRecord) => {
+      return g.values;
+    }),
+    projects: projects.map((p: TableRecord) => {
+      return p.values;
+    }),
+    schedules: schedules.map((s: TableRecord) => {
+      return s.values;
     })
+  });
 }
 
 function updateSchedule(scheduleJson) {
-    let app = initApp();
-    Logger.log(`updateSchedule: scheduleJson: ${scheduleJson}`)
-    let schedule = JSON.parse(scheduleJson);
-    let record = app.scheduleTable.findRecordByPrimaryKey(schedule[app.scheduleTable.primaryKey]);
-    record.values.start = moment(schedule.start.replace('Z', ''), moment.HTML5_FMT.DATETIME_LOCAL_MS).add(9, 'hours').format('YYYY/MM/DD');
-    record.values.end = moment(schedule.end.replace('Z', ''), moment.HTML5_FMT.DATETIME_LOCAL_MS).add(9, 'hours').format('YYYY/MM/DD');
-    Logger.log(`updateSchedule: try record.save`);
-    record.save();
+  let app = initApp();
+  Logger.log(`updateSchedule: scheduleJson: ${scheduleJson}`);
+  let schedule = JSON.parse(scheduleJson);
+  let record = app.scheduleTable.findRecordByPrimaryKey(
+    schedule[app.scheduleTable.primaryKey]
+  );
+  record.values.start = moment(
+    schedule.start.replace("Z", ""),
+    moment.HTML5_FMT.DATETIME_LOCAL_MS
+  )
+    .add(9, "hours")
+    .format("YYYY/MM/DD");
+  record.values.end = moment(
+    schedule.end.replace("Z", ""),
+    moment.HTML5_FMT.DATETIME_LOCAL_MS
+  )
+    .add(9, "hours")
+    .format("YYYY/MM/DD");
+  Logger.log(`updateSchedule: try record.save`);
+  record.save();
 }
 
 function onEdit(event) {
-    let app = initApp();
-    app.onEdit(event);
+  let app = initApp();
+  app.onEdit(event);
 }
 
 function resetTriggers() {
-    let triggers = ScriptApp.getProjectTriggers();
-    for (var i = 0; i < triggers.length; i++) {
-        ScriptApp.deleteTrigger(triggers[i]);
-    }
+  let triggers = ScriptApp.getProjectTriggers();
+  for (var i = 0; i < triggers.length; i++) {
+    ScriptApp.deleteTrigger(triggers[i]);
+  }
 
-    var sheet = SpreadsheetApp.getActive();
-    ScriptApp.newTrigger('onEdit')
-      .forSpreadsheet(sheet)
-      .onEdit()
-      .create();
+  var sheet = SpreadsheetApp.getActive();
+  ScriptApp.newTrigger("onEdit")
+    .forSpreadsheet(sheet)
+    .onEdit()
+    .create();
 }
 
 // ---------------------------------------------------
 
-const CONFIG_SHEET_NAME = 'Config';
-const LOG_SHEET_NAME = 'Log'
-const LOG_SHEET = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(LOG_SHEET_NAME);
-const LOG_MAX_ROWS = 2000
+const CONFIG_SHEET_NAME = "Config";
+const LOG_SHEET_NAME = "Log";
+const LOG_SHEET = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(
+  LOG_SHEET_NAME
+);
+const LOG_MAX_ROWS = 2000;
 
 function initApp(): App {
-    if(DEBUG) {
-        initDebugLogger();
-    }
-    Logger.log(`Init App`);
-    let config = new Config(CONFIG_SHEET_NAME, 2, 3, 'key')
-    let app = new App(config);
+  if (DEBUG) {
+    initDebugLogger();
+  }
+  Logger.log(`Init App`);
+  let config = new Config(CONFIG_SHEET_NAME, 2, 3, "key");
+  let app = new App(config);
 
-    Logger.log(`Init App successfully`);
-    return app
+  Logger.log(`Init App successfully`);
+  return app;
 }
 
 class Config {
-    public groupSheetName: string
-    public groupSheetHeaderRangeFirstRowNumber: number
-    public groupSheetRecordRangeFirstRowNumber: number
-    public groupPrimaryKey: string
+  public groupSheetName: string;
+  public groupSheetHeaderRangeFirstRowNumber: number;
+  public groupSheetRecordRangeFirstRowNumber: number;
+  public groupPrimaryKey: string;
 
-    public projectSheetName: string
-    public projectSheetHeaderRangeFirstRowNumber: number
-    public projectSheetRecordRangeFirstRowNumber: number
-    public projectPrimaryKey: string
+  public projectSheetName: string;
+  public projectSheetHeaderRangeFirstRowNumber: number;
+  public projectSheetRecordRangeFirstRowNumber: number;
+  public projectPrimaryKey: string;
 
-    public scheduleSheetName: string
-    public scheduleSheetHeaderRangeFirstRowNumber: number
-    public scheduleSheetRecordRangeFirstRowNumber: number
-    public schedulePrimaryKey: string
+  public scheduleSheetName: string;
+  public scheduleSheetHeaderRangeFirstRowNumber: number;
+  public scheduleSheetRecordRangeFirstRowNumber: number;
+  public schedulePrimaryKey: string;
 
-    constructor(sheetName: string, headerRangeFirstRowNumber: number, recordRangeFirstRowNumber: number, primaryKey: string) {
-        let configTable = new Table(sheetName, headerRangeFirstRowNumber, recordRangeFirstRowNumber, primaryKey);
-        let records = configTable.getAllRecords();
-        var configData = {};
-        records.forEach((record: TableRecord, index: number)=>{
-            Logger.log(`Config record: record.values: ${JSON.stringify(record.values)}`);
-            configData[record.values['key']] = record.values['value']; });
+  constructor(
+    sheetName: string,
+    headerRangeFirstRowNumber: number,
+    recordRangeFirstRowNumber: number,
+    primaryKey: string
+  ) {
+    let configTable = new Table(
+      sheetName,
+      headerRangeFirstRowNumber,
+      recordRangeFirstRowNumber,
+      primaryKey
+    );
+    let records = configTable.getAllRecords();
+    var configData = {};
+    records.forEach((record: TableRecord, index: number) => {
+      Logger.log(
+        `Config record: record.values: ${JSON.stringify(record.values)}`
+      );
+      configData[record.values["key"]] = record.values["value"];
+    });
 
-        Logger.log(`Config constructor: configData: ${JSON.stringify(configData)}`);
+    Logger.log(`Config constructor: configData: ${JSON.stringify(configData)}`);
 
-        this.groupSheetName = configData['groupSheetName'];
-        this.groupSheetHeaderRangeFirstRowNumber = configData['groupSheetHeaderRangeFirstRowNumber'];
-        this.groupSheetRecordRangeFirstRowNumber = configData['groupSheetRecordRangeFirstRowNumber'];
-        this.groupPrimaryKey = configData['groupPrimaryKey'];
+    this.groupSheetName = configData["groupSheetName"];
+    this.groupSheetHeaderRangeFirstRowNumber =
+      configData["groupSheetHeaderRangeFirstRowNumber"];
+    this.groupSheetRecordRangeFirstRowNumber =
+      configData["groupSheetRecordRangeFirstRowNumber"];
+    this.groupPrimaryKey = configData["groupPrimaryKey"];
 
-        this.projectSheetName = configData['projectSheetName'];
-        this.projectSheetHeaderRangeFirstRowNumber = configData['projectSheetHeaderRangeFirstRowNumber'];
-        this.projectSheetRecordRangeFirstRowNumber = configData['projectSheetRecordRangeFirstRowNumber'];
-        this.projectPrimaryKey = configData['projectPrimaryKey'];
+    this.projectSheetName = configData["projectSheetName"];
+    this.projectSheetHeaderRangeFirstRowNumber =
+      configData["projectSheetHeaderRangeFirstRowNumber"];
+    this.projectSheetRecordRangeFirstRowNumber =
+      configData["projectSheetRecordRangeFirstRowNumber"];
+    this.projectPrimaryKey = configData["projectPrimaryKey"];
 
-        this.scheduleSheetName = configData['scheduleSheetName'];
-        this.scheduleSheetHeaderRangeFirstRowNumber = configData['scheduleSheetHeaderRangeFirstRowNumber'];
-        this.scheduleSheetRecordRangeFirstRowNumber = configData['scheduleSheetRecordRangeFirstRowNumber'];
-        this.schedulePrimaryKey = configData['schedulePrimaryKey'];
-    }
+    this.scheduleSheetName = configData["scheduleSheetName"];
+    this.scheduleSheetHeaderRangeFirstRowNumber =
+      configData["scheduleSheetHeaderRangeFirstRowNumber"];
+    this.scheduleSheetRecordRangeFirstRowNumber =
+      configData["scheduleSheetRecordRangeFirstRowNumber"];
+    this.schedulePrimaryKey = configData["schedulePrimaryKey"];
+  }
 }
-
--- dist/models.ts
+++ dist/models.ts
@@ -1,140 +1,163 @@
 // moment を使えるようにするには以下が必要
 // また方などは適宜 import('moment').Moment などして利用する
-declare let moment: typeof import('moment');
+declare let moment: typeof import("moment");
 
 class Table {
-    readonly sheetName: string
-    readonly sheetId: number
-    readonly headers: Array<string>
-    readonly primaryKey: string
+  readonly sheetName: string;
+  readonly sheetId: number;
+  readonly headers: Array<string>;
+  readonly primaryKey: string;
 
-    private headerRangeFirstRowNumber: number
-    private recordRangeFirstRowNumber: number
-    private sheetObj: GoogleAppsScript.Spreadsheet.Sheet
-    private primaryKeyColumnNumber: number
+  private headerRangeFirstRowNumber: number;
+  private recordRangeFirstRowNumber: number;
+  private sheetObj: GoogleAppsScript.Spreadsheet.Sheet;
+  private primaryKeyColumnNumber: number;
 
-    constructor(sheetName: string, headerRangeFirstRowNumber: number, recordRangeFirstRowNumber: number, primaryKey: string) {
-            this.sheetName = sheetName;
-            this.sheetObj = SpreadsheetApp.getActive().getSheetByName(sheetName);
-            this.sheetId = this.sheetObj.getSheetId();
-            this.headerRangeFirstRowNumber = headerRangeFirstRowNumber;
-            this.recordRangeFirstRowNumber = recordRangeFirstRowNumber;
-            this.primaryKey = primaryKey;
+  constructor(
+    sheetName: string,
+    headerRangeFirstRowNumber: number,
+    recordRangeFirstRowNumber: number,
+    primaryKey: string
+  ) {
+    this.sheetName = sheetName;
+    this.sheetObj = SpreadsheetApp.getActive().getSheetByName(sheetName);
+    this.sheetId = this.sheetObj.getSheetId();
+    this.headerRangeFirstRowNumber = headerRangeFirstRowNumber;
+    this.recordRangeFirstRowNumber = recordRangeFirstRowNumber;
+    this.primaryKey = primaryKey;
 
-            // header の数は最大 20
-            let headers = <Array<string>>this.sheetObj.getRange(headerRangeFirstRowNumber, 1, 1, 20).getValues()[0];
-            this.headers = [];
-            headers.forEach((v)=>{ if(v !== undefined && v !== '') this.headers.push(v) });
+    // header の数は最大 20
+    let headers = <Array<string>>(
+      this.sheetObj.getRange(headerRangeFirstRowNumber, 1, 1, 20).getValues()[0]
+    );
+    this.headers = [];
+    headers.forEach(v => {
+      if (v !== undefined && v !== "") this.headers.push(v);
+    });
 
-            let index = this.headers.indexOf(primaryKey);
-            if (index === -1) {
-                new Error(`${sheetName} の primaryKey:${primaryKey} が存在しません`);
-            }
-            this.primaryKeyColumnNumber = index + 1;
+    let index = this.headers.indexOf(primaryKey);
+    if (index === -1) {
+      new Error(`${sheetName} の primaryKey:${primaryKey} が存在しません`);
     }
+    this.primaryKeyColumnNumber = index + 1;
+  }
 
-    public newRecordWithRowNumber(rowNumber: number) {
-        let range = this.sheetObj.getRange(rowNumber, 1, 1, this.headers.length);
-        return new TableRecord(this, range);
+  public newRecordWithRowNumber(rowNumber: number) {
+    let range = this.sheetObj.getRange(rowNumber, 1, 1, this.headers.length);
+    return new TableRecord(this, range);
+  }
+
+  public insertRecord(row: TableRecord) {
+    // this.sheetObj.appendRow(this.rowTowValues(row));
+  }
+
+  public getAllRecords(): Array<TableRecord> {
+    let lastRowNumber = this.getLastRowNumber();
+    let rows = [];
+    for (var i = this.recordRangeFirstRowNumber; i <= lastRowNumber; i++) {
+      let record = new TableRecord(this, this.getRangeByRowNumber(i));
+      if (record.hasPrimaryKey()) {
+        rows.push(record);
+      }
     }
+    return rows;
+  }
 
-    public insertRecord(row: TableRecord) {
-        // this.sheetObj.appendRow(this.rowTowValues(row));
+  public findRecordByPrimaryKey(key: string): TableRecord | null {
+    let range = this.findRangeByPrimaryKey(key);
+    if (range == null) {
+      return null;
     }
+    return new TableRecord(this, range);
+  }
 
-    public getAllRecords(): Array<TableRecord> {
-        let lastRowNumber = this.getLastRowNumber();
-        let rows = [];
-        for (var i = this.recordRangeFirstRowNumber; i <= lastRowNumber; i++) {
-            let record = new TableRecord(this, this.getRangeByRowNumber(i));
-            if(record.hasPrimaryKey()) {
-                rows.push(record);
-            }
-        }
-        return rows;
+  private getLastRowNumber(): number {
+    let range = this.getPrimaryKeyColRange();
+    Logger.log(
+      `getLastRecordRowNumber: range.getLastRow: ${range.getLastRow()}`
+    );
+    return range.getLastRow() + this.recordRangeFirstRowNumber - 1;
+  }
+
+  // public getLastRecord(): TableRecord {
+  //     return new TableRecord(this, this.getRangeByRowNumber(this.getLastRecordRowNumber()));
+  // }
+
+  private getRangeByRowNumber(
+    rowNumber: number
+  ): GoogleAppsScript.Spreadsheet.Range {
+    return this.sheetObj.getRange(rowNumber, 1, 1, this.headers.length);
+  }
+
+  private findRangeByPrimaryKey(
+    key: string
+  ): GoogleAppsScript.Spreadsheet.Range {
+    let textFinder = this.getPrimaryKeyColRange().createTextFinder(key);
+    let range = textFinder.findNext();
+    if (range == null) {
+      return null;
     }
+    return this.sheetObj.getRange(range.getRow(), 1, 1, this.headers.length);
+  }
 
-    public findRecordByPrimaryKey(key: string): TableRecord | null {
-        let range = this.findRangeByPrimaryKey(key);
-        if(range == null) {
-            return null;
-        }
-        return new TableRecord(this, range);
-    }
-
-    private getLastRowNumber(): number {
-        let range = this.getPrimaryKeyColRange();
-        Logger.log(`getLastRecordRowNumber: range.getLastRow: ${range.getLastRow()}`);
-        return range.getLastRow() + this.recordRangeFirstRowNumber - 1;
-    }
-
-    // public getLastRecord(): TableRecord {
-    //     return new TableRecord(this, this.getRangeByRowNumber(this.getLastRecordRowNumber()));
-    // }
-
-    private getRangeByRowNumber(rowNumber: number): GoogleAppsScript.Spreadsheet.Range {
-        return this.sheetObj.getRange(rowNumber, 1, 1, this.headers.length);
-    }
-
-    private findRangeByPrimaryKey(key: string): GoogleAppsScript.Spreadsheet.Range {
-        let textFinder = this.getPrimaryKeyColRange().createTextFinder(key);
-        let range = textFinder.findNext();
-        if(range == null) {
-            return null;
-        }
-        return this.sheetObj.getRange(range.getRow(), 1, 1, this.headers.length);
-    }
-
-    private getPrimaryKeyColRange(): GoogleAppsScript.Spreadsheet.Range {
-        return this.sheetObj.getRange(this.recordRangeFirstRowNumber, this.primaryKeyColumnNumber,
-            this.sheetObj.getLastRow() - this.recordRangeFirstRowNumber, 1);
-    }
+  private getPrimaryKeyColRange(): GoogleAppsScript.Spreadsheet.Range {
+    return this.sheetObj.getRange(
+      this.recordRangeFirstRowNumber,
+      this.primaryKeyColumnNumber,
+      this.sheetObj.getLastRow() - this.recordRangeFirstRowNumber,
+      1
+    );
+  }
 }
 
 class TableRecord {
-    public table: Table
-    public range: GoogleAppsScript.Spreadsheet.Range
-    public values: Object
+  public table: Table;
+  public range: GoogleAppsScript.Spreadsheet.Range;
+  public values: Object;
 
-    constructor(table: Table, range: GoogleAppsScript.Spreadsheet.Range) {
-        this.table = table;
-        this.range = range;
-        let rangeVals = this.range.getValues()[0];
-        let values = {};
-        this.table.headers.forEach((k: string, i: number)=>{ values[k] = rangeVals[i]});
-        this.values = values;
-    }
+  constructor(table: Table, range: GoogleAppsScript.Spreadsheet.Range) {
+    this.table = table;
+    this.range = range;
+    let rangeVals = this.range.getValues()[0];
+    let values = {};
+    this.table.headers.forEach((k: string, i: number) => {
+      values[k] = rangeVals[i];
+    });
+    this.values = values;
+  }
 
-    public hasPrimaryKey(): boolean {
-        let primaryKey = this.values[this.table.primaryKey];
-        Logger.log(`hasPrimaryKey: ${primaryKey}`);
-        return !isBlank(primaryKey);
-    }
+  public hasPrimaryKey(): boolean {
+    let primaryKey = this.values[this.table.primaryKey];
+    Logger.log(`hasPrimaryKey: ${primaryKey}`);
+    return !isBlank(primaryKey);
+  }
 
-    public save() {
-        this.range.setValues([this.valueArray()]);
-    }
+  public save() {
+    this.range.setValues([this.valueArray()]);
+  }
 
-    // public saveAsNewRecord() {
-    //     Logger.log(`saveAsNewRecord: primaryKeyName: ${this.table.primaryKey}`)
-    //     this.values[this.table.primaryKey] = this.getNewKey();
-    //     Logger.log(`saveAsNewRecord: new values: ${this.values}`)
-    //     this.range.setValues([this.valueArray()]);
-    // }
+  // public saveAsNewRecord() {
+  //     Logger.log(`saveAsNewRecord: primaryKeyName: ${this.table.primaryKey}`)
+  //     this.values[this.table.primaryKey] = this.getNewKey();
+  //     Logger.log(`saveAsNewRecord: new values: ${this.values}`)
+  //     this.range.setValues([this.valueArray()]);
+  // }
 
-    // public setPrimaryKey() {
-    //     this.range.getCell(1, 1).setValue(this.getNewKey());
-    // }
+  // public setPrimaryKey() {
+  //     this.range.getCell(1, 1).setValue(this.getNewKey());
+  // }
 
-    private valueArray(): Array<any>{
-        let valueAry: Array<any> = [];
-        let values = this.values;
-        this.table.headers.forEach((k: string)=>{ valueAry.push(values[k]); });
-        Logger.log(`valueArray: ${valueAry}`)
-        return valueAry;
-    }
+  private valueArray(): Array<any> {
+    let valueAry: Array<any> = [];
+    let values = this.values;
+    this.table.headers.forEach((k: string) => {
+      valueAry.push(values[k]);
+    });
+    Logger.log(`valueArray: ${valueAry}`);
+    return valueAry;
+  }
 
-    // private getNewKey(): string {
-    //     return Utilities.getUuid();
-    // }
+  // private getNewKey(): string {
+  //     return Utilities.getUuid();
+  // }
 }
