f050da3cfddd07ee7492f14d9231b9b0989d4319
kazukgw <kazukgw@gmail.com>
2020-04-18 00:41:28 +0900

    Update

--- src/utils.ts
+++ src/utils.ts
@@ -2,17 +2,3 @@ function includeHtml(filename) {
   return HtmlService.createHtmlOutputFromFile(filename).getContent();
 }
 
-function deleteAllTriggers() {
-  let triggers = ScriptApp.getProjectTriggers();
-  for (var i = 0; i < triggers.length; i++) {
-    ScriptApp.deleteTrigger(triggers[i]);
-  }
-
-  // onEdit という関数は自動でトリガ設定されるので手動での設定は不要
-  //
-  // var sheet = SpreadsheetApp.getActive();
-  // ScriptApp.newTrigger('onEdit')
-  //   .forSpreadsheet(sheet)
-  //   .onEdit()
-  //   .create();
-}
